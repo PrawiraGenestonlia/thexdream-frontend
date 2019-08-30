e6a86c77549d2bef3ee577e86a06b65a60bb2a89
PrawiraGenestonlia <prawira.96@gmail.com>
2019-08-31 01:10:54 +0800

    .

--- src/pages/Topnews.page.js
+++ src/pages/Topnews.page.js
@@ -1,15 +1,43 @@
 import React, { Fragment, useState, useEffect } from 'reactn';
 import axios from 'axios';
+// import request from "superagent";
+import debounce from "lodash.debounce";
 import config from '../config';
 import { NewsCard } from '../components';
 
 export default () => {
   const [currentnews, setCurrentNews] = useState('');
+  const [loadedArticles, setLoadedArticles] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // window.addEventListener("scroll", debounce(() => {
+  //   if (
+  //     (window.innerHeight + document.documentElement.scrollTop)
+  //     >= document.documentElement.offsetHeight
+  //   ) {
+  //     setLoadedArticles(loadedArticles + 2);
+  //   }
+  // }, 100));
+
+  window.onscroll = debounce(() => {
+    if (isLoading || !hasMore) return;
+    if (
+      (window.innerHeight + document.documentElement.scrollTop)
+      === document.documentElement.offsetHeight
+    ) {
+      setLoadedArticles(loadedArticles + 2);
+      // setIsLoading(true);
+      if (loadedArticles === currentnews.totalResults) setHasMore(false);
+    }
+  }, 100);
+
   useEffect(() => {
     axios.get(config.server_url + config.api.getTopNews)
       .then(res => {
         // console.log(res);
         res.status === 200 && setCurrentNews(res.data);
+        res.status === 200 && setLoadedArticles(5);
       })
       .catch(err => {
         alert(err);
@@ -19,8 +47,26 @@ export default () => {
 
   return (
     <Fragment>
-      {currentnews ? <NewsCard data={currentnews.articles[0]}/> : null}
-      <span>Top News</span>
+      {
+        currentnews && currentnews.articles.map((articles, index) => {
+          if (index < loadedArticles) return <NewsCard data={articles} />
+          else return null;
+        })
+      }
+      <p>
+        {window.scrollTop};{window.clientHeight}
+        ;{window.scrollHeight}
+      </p>
+      {isLoading &&
+        <div>Loading...</div>
+      }
+      {/* {
+        isLoading &&
+        setTimeout(() => { setIsLoading(false) }, 500)
+      } */}
+      {!hasMore &&
+        <div>You did it! You reached the end!</div>
+      }
     </Fragment>
   )
 }
