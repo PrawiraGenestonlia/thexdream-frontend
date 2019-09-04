f6e750ac31f68fe0441818c7285ee54a7cfaeedc
PrawiraGenestonlia <prawira.96@gmail.com>
2019-09-04 18:42:14 +0800

    .

--- src/pages/Homepage.page.js
+++ src/pages/Homepage.page.js
@@ -7,13 +7,19 @@ export default () => {
   };
   return (
     <Fragment>
-      <span>Home Page</span>
-      <p>
-        current react version {process.env.REACT_APP_GIT_SHA}
-      </p>
-      <p>
-        <button onClick={refresh}>refresh</button>
-      </p>
+      <div style={{ alignItems: 'center', justifySelf: 'center' }}>
+        <p style={{ textAlign: 'center' }}>
+          This application is still undergoing development. You can start using the
+          news
+          function (navigate through the menu)
+        </p>
+        <p style={{ textAlign: 'center' }}>
+          Contact <a>praw0001@e.ntu.edu.sg</a> for any suggestions.
+        </p>
+      </div>
+
+      {/* <p>current version {process.env.REACT_APP_GIT_SHA}</p> */}
+      {/* <p><button onClick={refresh}>refresh</button>s</p> */}
     </Fragment>
   )
 }
